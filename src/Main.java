7e9533a811d98792f9ca66c589f5ae575b96a781
AAARRR0412 <anrobot0412@gmail.com>
2023-05-30 22:19:10 +0900

    주석 추가

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.text.*;
 
 public class Main {
     public static void main(String arg[]) {
-        Menu menu = new Menu(); // 시작하면 메인 메뉴로 이동
+        Menu menu = new Menu(); // 시작하면 메인 메뉴로 이동합니다.
         menu.home(menu.cart);
     }
 }
@@ -25,6 +25,15 @@ class Menu {
         this.explain = b;
     } // 기본 생성자
 
+    /// 메인 화면에 사용되는 객체들 입니다. (선택지)
+    static Menu burgers = new Menu("Burgers","앵거스 비프 통살을 다져만든 버거" );
+    static Menu custard = new Menu("Forzen Custard","매장에서 신선하게 만드는 아이스크림" );
+    static Menu drink = new Menu("Drinks","매장에서 직접 만드는 음료" );
+    static Menu beer = new Menu("Beer","뉴욕 브루클린 브루어리에서 양조한 맥주" );
+    static Menu order = new Menu("Order","장바구니를 확인 후 주문합니다." );
+    static Menu cancel = new Menu("Cancel","진행중인 주문을 취소합니다." );
+
+    /// 입력을 받는 메서드 입니다.
     static int choose(int a) {
 
         Scanner sc = new Scanner(System.in);
@@ -51,16 +60,13 @@ class Menu {
             }
         }
         return result;
-    } //
+    }
 
-    static Menu burgers = new Menu("Burgers","앵거스 비프 통살을 다져만든 버거" );
-    static Menu custard = new Menu("Forzen Custard","매장에서 신선하게 만드는 아이스크림" );
-    static Menu drink = new Menu("Drinks","매장에서 직접 만드는 음료" );
-    static Menu beer = new Menu("Beer","뉴욕 브루클린 브루어리에서 양조한 맥주" );
-    static Menu order = new Menu("Order","장바구니를 확인 후 주문합니다." );
-    static Menu cancel = new Menu("Cancel","진행중인 주문을 취소합니다." );
+    ///
 
-    Cart cart = new Cart();
+    Cart cart = new Cart(); // 메뉴 클래스에서 사용 할 장바구니 객체를 만듭니다. (장바구니에 물건을 전달 받아야 함)
+
+    // 메인 화면 메서드 입니다.
     public void home(Cart cart) {
         System.out.println("\"SHAKESHACK BURGER 에 오신걸 환영합니다.\"");
         System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
@@ -101,13 +107,18 @@ class Menu {
         }
     }
 
+    /// 메인화면 메서드 끝입니다.
+
     private static int orderCount = 0; // 주문 횟수 (대기번호)
+
+    /// 주문화면 메서드 시작입니다.
+
     private void Show_Cart(Cart cart) {
         System.out.println("\n[ 장바구니 ]");
         for (Order order : cart.getOrders()) {
             System.out.println("상품명 : " + order.getName() + " | 가격 : W " + order.getPrice());
         }
-        DecimalFormat df = new DecimalFormat("#,###.#");
+        DecimalFormat df = new DecimalFormat("#,###.#"); /// 단위를 소숫점 1자리로 설정합니다.
         System.out.println("총합 : W " + df.format(cart.getTotal()));
         System.out.println("\n아래와 같이 주문 하시겠습니까? (Y/N)");
 
@@ -126,6 +137,10 @@ class Menu {
         }
     }
 
+    /// 주문화면 메서드 끝입니다.
+
+    /// 장바구니 비우고 메인으로 돌아가는 메서드 입니다.
+
     private void Clear_Cart(Cart cart) {
         cart.clearorders();
         System.out.println("장바구니가 비워졌습니다.");
@@ -135,7 +150,6 @@ class Menu {
 
 class Item extends Menu {
     double price; // 가격
-
     public String toString() {
         return name + " | W : " + price + " | " + explain;
     }
@@ -147,7 +161,27 @@ class Item extends Menu {
         this.price = c;
     }
 
-  // [ order 클래스의 price, name 변수와 Item 클래스의 price와 name 변수를 일치 시켜주는 메서드 ]
+    /// [Burgers]
+    static Item shack = new Item("Shack Burger", "토마토, 양상추, 쉑소스가 토핑된 치즈버거", 6.9);
+    static Item smoke = new Item("Smoke Shack", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거", 8.9);
+    static Item shroom = new Item("Shroom Burger", "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거", 9.4);
+    static Item cheese = new Item("Cheese burger", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거", 6.9);
+    static Item clasic = new Item("Hamburger", "비프패티를 기반으로 야채가 들어간 기본버거", 5.4);
+    /// [Forzen Custards]
+    static Item vanilla = new Item("VaniLla Frozen Custard", "콕콕 씹히는 바닐라빈의 향기를 머금은 쫀득한 아이스크림", 3.5);
+    static Item strawberry = new Item("Strawberry Frozen Custard", "쫀득한 젤라또 기반의 달콤한 아이스크림에 상큼한 딸기가 퐁당", 3.2);
+    static Item choco = new Item("Choco Frozen Custard", "쫀득한 아이스크림과 초콜릿의 달콤한 콜라보", 3.0);
+    /// [Drinks]
+    static Item coke = new Item("Coke", "짜릿한 탄산과 다가오는 달콤함. 햄버거의 영원한 단짝", 1.5);
+    static Item soda = new Item("Lime soda", "상큼한 레몬과 라임 그리고 탄산의 청량한 만남", 1.3);
+    static Item ambasa = new Item("Ambasa", "부드러운 우유와 탄산의 달콤한 콜라보레이션", 1.1);
+    /// [Beers]
+    static Item kas = new Item("Kas", "카스를 안 마셔본 사람은 있지만, 한 번만 마셔본 사람은 없다는 한국의 대표 맥주", 2.3);
+    static Item hite = new Item("Hite", "부드럽고 꺼지지 않는 거품이 특징인 청량한 맥주", 2.2);
+    static Item filite = new Item("Fi-lite", "세상에서 가장 싼 맥주, 세상에서 가장 무난한 맥주", 1.4);
+
+
+    /// [ order 클래스의 price, name 변수와 Item 클래스의 price와 name 변수를 일치 시켜주는 메서드 ]
 
     public double getPrice() {
         return this.price;
@@ -156,14 +190,9 @@ class Item extends Menu {
         return this.name;
     }
 
-  //
+    ///
 
-    static Item shack = new Item("Shack Burger", "토마토, 양상추, 쉑소스가 토핑된 치즈버거", 6.9);
-    static Item smoke = new Item("Smoke Shack", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거", 8.9);
-    static Item shroom = new Item("Shroom Burger", "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거", 9.4);
-    static Item cheese = new Item("Cheese burger", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거", 6.9);
-    static Item clasic = new Item("Hamburger", "비프패티를 기반으로 야채가 들어간 기본버거", 5.4);
-
+    /// 버거 선택 화면 메서드 입니다.
     public void Burger_Menu(Cart cart) {
         System.out.println("\"SHAKESHACK BURGER 에 오신걸 환영합니다.\"");
         System.out.println("아래 상품 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
@@ -200,9 +229,7 @@ class Item extends Menu {
         menu.home(cart);
     }
 
-    static Item vanilla = new Item("VaniLla Forzen Custard", "콕콕 씹히는 바닐라빈의 향기를 머금은 쫀득한 아이스크림", 3.5);
-    static Item strawberry = new Item("Strawberry Forzen Custard", "쫀득한 젤라또 기반의 달콤한 아이스크림에 상큼한 딸기가 퐁당", 3.2);
-    static Item choco = new Item("Choco Forzen Custard", "쫀득한 아이스크림과 초콜릿의 달콤한 콜라보", 3.0);
+    /// 아이스크림 선택 화면 메서드 입니다.
     public void Icecream_Menu(Cart cart) {
         System.out.println("\"SHAKESHACK BURGER 에 오신걸 환영합니다.\"");
         System.out.println("아래 상품 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
@@ -231,10 +258,7 @@ class Item extends Menu {
         menu.home(cart);
     }
 
-    static Item coke = new Item("Coke", "짜릿한 탄산과 다가오는 달콤함. 햄버거의 영원한 단짝", 1.5);
-    static Item soda = new Item("Lime soda", "상큼한 레몬과 라임 그리고 탄산의 청량한 만남", 1.3);
-    static Item ambasa = new Item("Ambasa", "부드러운 우유와 탄산의 달콤한 콜라보레이션", 1.1);
-
+    /// 음료수 선택 화면 메서드 입니다.
     public void Drink_Menu(Cart cart) {
         System.out.println("\"SHAKESHACK BURGER 에 오신걸 환영합니다.\"");
         System.out.println("아래 상품 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
@@ -263,9 +287,7 @@ class Item extends Menu {
         menu.home(cart);
     }
 
-    static Item kas = new Item("Kas", "카스를 안 마셔본 사람은 있지만, 한 번만 마셔본 사람은 없다는 한국의 대표 맥주", 2.3);
-    static Item hite = new Item("Hite", "부드럽고 꺼지지 않는 거품이 특징인 청량한 맥주", 2.2);
-    static Item filite = new Item("Fi-lite", "세상에서 가장 싼 맥주, 세상에서 가장 무난한 맥주", 1.4);
+    /// 맥주 선택 화면 메서드 입니다.
     public void Beer_Menu(Cart cart) {
         System.out.println("\"SHAKESHACK BURGER 에 오신걸 환영합니다.\"");
         System.out.println("아래 상품 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
@@ -277,7 +299,6 @@ class Item extends Menu {
 
         int max = 3;
         int choose = Menu.choose(max);
-        Order order = new Order();
 
         switch (choose) {
             case 1:
@@ -297,19 +318,20 @@ class Item extends Menu {
 }
 
 class Cart {
+    /// Order 클래스의 객체만을 담을 수 있는 Arraylist 를 생성합니다. 지네렉스 <>를 통해서 Order 클래스만 담을 수 있게 했습니다.
     private ArrayList<Order> orders;
     public Cart() {
         this.orders = new ArrayList<>();
     }
-    public void addOrder(Order order) {
+    public void addOrder(Order order) { // Order 클래스의 order 객체를 Arraylist orders에 추가합니다.
         this.orders.add(order);
     }
     public void clearorders() {
         this.orders.clear();
-    }
+    } /// Arraylist orders를 초기화 합니다.
     public ArrayList<Order> getOrders() {
         return this.orders;
-    }
+    } /// 주문화면 메서드에서 Arraylist orders에 담긴 것을 전달합니다.
 
     public double getTotal() {
         double total = 0;
@@ -317,29 +339,29 @@ class Cart {
             total += order.getPrice();
         }
         return total;
-    }
+    } /// Order 클래스의 order 객체의 price 값을 모두 더합니다.
 
 }
 class Order {
-    String name; // 이름
-    double price; // 가격
+    String name; // 주문한 상품의 이름을 담습니다.
+    double price; // 주문한 상품의 가격을 담습니다.
 
     public Order(String name, double price) {
         this.name = name;
         this.price = price;
-    }
+    } /// Item 클래스의 name, price를 Order 클래스의 name, price로 설정할 때 사용한 메서드입니다.
 
     public void setPrice(double price) {
         this.price = price;
-    }
+    } /// Item 클래스의 name를 Order 클래스의 name로 설정할 떄 사용한 메서드입니다.
     public void setName(String name) {
         this.name = name;
-    }
+    } /// Item 클래스의 name를 Order 클래스의 name로 설정할 떄 사용한 메서드입니다.
     public double getPrice() {
         return this.price;
-    }
+    } /// price를 읽는 메서드 입니다.
     public String getName() {
         return this.name;
-    }
+    } /// name를 읽는 메서드 입니다.
 
 }
