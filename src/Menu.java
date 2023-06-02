f3a5cbbec416cab693c6bbcfe71982eb52152c12
AAARRR0412 <anrobot0412@gmail.com>
2023-06-02 17:34:44 +0900

    ArrayList 사용을 통한 뭐시기 뭐시기..

--- src/Menu.java
+++ src/Menu.java
@@ -1,27 +1,29 @@
 import java.text.DecimalFormat;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Menu {
 
     String name; // 이름
     String explain; // 설명
+    ArrayList<Menu> menuList = new ArrayList<>();
 
     public String toString() {
         return name + " | " + explain;
     }
 
-    Menu() {}
+    Menu() {
+        menuList.add(new Menu("Burgers","앵거스 비프 통살을 다져만든 버거"));
+        menuList.add(new Menu("Forzen Custard","매장에서 신선하게 만드는 아이스크림"));
+        menuList.add(new Menu("Drinks","매장에서 직접 만드는 음료"));
+        menuList.add(new Menu("Beer","뉴욕 브루클린 브루어리에서 양조한 맥주"));
+    }
     Menu(String a, String b) {
         this.name = a;
         this.explain = b;
     } // 기본 생성자
 
-    /// 메인 화면에 사용되는 객체들 입니다. (선택지)
-    static Menu burgers = new Menu("Burgers","앵거스 비프 통살을 다져만든 버거" );
-    static Menu custard = new Menu("Forzen Custard","매장에서 신선하게 만드는 아이스크림" );
-    static Menu drink = new Menu("Drinks","매장에서 직접 만드는 음료" );
-    static Menu beer = new Menu("Beer","뉴욕 브루클린 브루어리에서 양조한 맥주" );
     /// 입력을 받는 메서드 입니다.
     static int choose(int a) {
 
@@ -61,14 +63,13 @@ public class Menu {
         System.out.println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.");
         System.out.println("");
         System.out.println("[ SHAKESHACK MENU ]");
-        System.out.println("1. " + burgers.toString());
-        System.out.println("2. " + custard.toString());
-        System.out.println("3. " + drink.toString());
-        System.out.println("4. " + beer.toString());
+        for (int i = 0; i < menuList.size(); i++) {
+            System.out.println((i + 1) + ". " + menuList.get(i).toString());
+        }
         System.out.println("");
         System.out.println("[ ORDER MENU ]");
-        System.out.println("Order | 장바구니를 확인 후 주문합니다. ");
-        System.out.println("Cancel | 진행중인 주문을 취소합니다.");
+        System.out.println("5. Order | 장바구니를 확인 후 주문합니다. ");
+        System.out.println("6. Cancel | 진행중인 주문을 취소합니다.");
 
         Item item = new Item();
 
